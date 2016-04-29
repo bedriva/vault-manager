a5e04a367288b7b523793a241ada39276b9eb06f
Magnus Johansson <m.f.johansson@gmail.com>
2016-04-29 11:37:32 +0200

    Updates

--- app/app.ts
+++ app/app.ts
@@ -16,8 +16,7 @@ import { HTTP_PROVIDERS } from 'angular2/http';
 import 'rxjs/Rx';
 
 import {provide}           from 'angular2/core';
-import {LocationStrategy,
-        HashLocationStrategy} from 'angular2/router';
+import {LocationStrategy, HashLocationStrategy} from 'angular2/router';
 
 var PATHS_CACHE = [];
 var MAX_LEVEL = false;
@@ -48,6 +47,20 @@ export class VaultService {
                     .map(this.extractDataSingle)
                     .catch(this.handleError);
   }
+  saveItem (path, data, token): Observable<any> {
+
+    var headers = new Headers();
+    headers.append('X-Vault-Token', token);
+
+    var post = {};
+    for (let i in data) {
+      post[data[i].key] = data[i].data;
+    }
+
+    return this.http.post('http://127.0.0.1:8200/v1/secret' + path + '', JSON.stringify(post), {headers: headers})
+                    .map(data => {return data;})
+                    .catch(this.handleError);
+  }
   private extractData(res: Response) {
     if (res.status < 200 || res.status >= 300) {
       throw new Error('Bad response status: ' + res.status);
@@ -61,7 +74,7 @@ export class VaultService {
       host: 'storage',
       user: 'person',
       pass: 'fingerprint',
-      key: 'vpn key',
+      key: 'vpn_key',
       port: 'dialpad',
       email: 'email',
       url: 'link',
@@ -70,7 +83,7 @@ export class VaultService {
     if (icons[key]) {
       return icons[key];
     } else {
-      return '<i class="material-icons">radio_button_unchecked</i>';
+      return 'radio_button_unchecked';
     }
   }
   private extractDataSingle(res: Response) {
@@ -164,59 +177,74 @@ export class VaultFolder {
     </div>
   </form>
 
-  <div *ngIf="tree" class="path-col path-col-root">
-    <span (click)="loadChildren(tree)">{{tree.name}} (root)</span>
-    <div class="path-col path-col-1" *ngFor="#child1 of tree.children">
-      <i class="material-icons">{{child1.folder ? 'folder' : 'description'}}</i> <span (click)="loadChildren(child1)">{{child1.name}}</span>
-      <div class="path-col path-col-2" *ngFor="#child2 of child1.children">
-        <i class="material-icons">{{child2.folder ? 'folder' : 'description'}}</i> <span (click)="loadChildren(child2)">{{child2.name}}</span>
-        <div class="path-col path-col-3" *ngFor="#child3 of child2.children">
-          <i class="material-icons">{{child3.folder ? 'folder' : 'description'}}</i> <span (click)="loadChildren(child3)">{{child3.name}}</span>
-          <div class="path-col path-col-4" *ngFor="#child4 of child3.children">
-            <i class="material-icons">{{child4.folder ? 'folder' : 'description'}}</i> <span (click)="loadChildren(child4)">{{child4.name}}</span>
-            <div class="path-col path-col-5" *ngFor="#child5 of child4.children">
-              <i class="material-icons">{{child5.folder ? 'folder' : 'description'}}</i> <span (click)="loadChildren(child5)">{{child5.name}}</span>
-              <div class="path-col path-col-6" *ngFor="#child6 of child5.children">
-                <i class="material-icons">{{child6.folder ? 'folder' : 'description'}}</i> <span (click)="loadChildren(child6)">{{child6.name}}</span>
+  <div class="left-col left-col-{{singleData ? 'half' : 'full'}}">
+
+    <div *ngIf="tree" class="path-col path-col-root">
+      <span (click)="loadChildren(tree)">{{tree.name}} (root)</span>
+      <div class="path-col path-col-1" *ngFor="#child1 of tree.children">
+        <i class="material-icons">{{child1.folder ? 'folder' : 'description'}}</i> <span (click)="loadChildren(child1)">{{child1.name}}</span>
+        <div class="path-col path-col-2" *ngFor="#child2 of child1.children">
+          <i class="material-icons">{{child2.folder ? 'folder' : 'description'}}</i> <span (click)="loadChildren(child2)">{{child2.name}}</span>
+          <div class="path-col path-col-3" *ngFor="#child3 of child2.children">
+            <i class="material-icons">{{child3.folder ? 'folder' : 'description'}}</i> <span (click)="loadChildren(child3)">{{child3.name}}</span>
+            <div class="path-col path-col-4" *ngFor="#child4 of child3.children">
+              <i class="material-icons">{{child4.folder ? 'folder' : 'description'}}</i> <span (click)="loadChildren(child4)">{{child4.name}}</span>
+              <div class="path-col path-col-5" *ngFor="#child5 of child4.children">
+                <i class="material-icons">{{child5.folder ? 'folder' : 'description'}}</i> <span (click)="loadChildren(child5)">{{child5.name}}</span>
+                <div class="path-col path-col-6" *ngFor="#child6 of child5.children">
+                  <i class="material-icons">{{child6.folder ? 'folder' : 'description'}}</i> <span (click)="loadChildren(child6)">{{child6.name}}</span>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
     </div>
+
   </div>
 
-  <div class="demo-card-wide mdl-card mdl-shadow--2dp" *ngIf="singleData">
-    <div class="mdl-card__title">
-      <h2 class="mdl-card__title-text">Data</h2>
-    </div>
-    <div class="mdl-card__supporting-text">
+  <div class="right-col" *ngIf="singleData">
 
-      <ul class="mdl-list">
-        <li class="mdl-list__item mdl-list__item--two-line" *ngFor="#item of singleData">
-          <span class="mdl-list__item-primary-content">
-            <i class="material-icons mdl-list__item-avatar">{{item.icon}}</i>
-            <span>{{item.data}}</span>
-            <span class="mdl-list__item-sub-title">{{item.key}}</span>
-          </span>
-        </li>
-      </ul>
+    <div class="mdl-card mdl-shadow--2dp {{editMode ? 'card-edit-mode' : 'card-read-mode'}}">
+      <div class="mdl-card__title">
+        <h2 class="mdl-card__title-text">Data</h2>
+      </div>
+      <div class="mdl-card__supporting-text">
 
+        <ul class="mdl-list">
+          <li class="mdl-list__item mdl-list__item--two-line" *ngFor="#item of singleData" (click)="clipboard(item.data)">
+            <span class="mdl-list__item-primary-content">
+              <i class="material-icons mdl-list__item-avatar">{{item.icon}}</i>
+              <span><input readonly="{{editMode ? '' : 'yes'}}" [(ngModel)]=item.data class="mdl-textfield__input" type="text"></span>
+              <span class="mdl-list__item-sub-title"><input readonly="{{editMode ? '' : 'yes'}}" [(ngModel)]=item.key class="mdl-textfield__input" type="text"></span>
+            </span>
+          </li>
+        </ul>
+
+      </div>
+      <div class="mdl-card__actions mdl-card--border" *ngIf="!editMode">
+        <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect" (click)="startEdit()">
+          Edit
+        </a>
+      </div>
+      <div class="mdl-card__actions mdl-card--border" *ngIf="editMode">
+        <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect" (click)="saveEdit()">
+          Save
+        </a>
+        <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect" (click)="cancelEdit()">
+          Cancel
+        </a>
+      </div>
+      <div class="mdl-card__menu">
+        <button class="mdl-button mdl-button--icon mdl-js-button mdl-js-ripple-effect">
+          <i class="material-icons">copy</i>
+        </button>
+      </div>
     </div>
-    <div class="mdl-card__actions mdl-card--border">
-      <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
-        Edit
-      </a>
-      <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
-        More
-      </a>
-    </div>
-    <div class="mdl-card__menu">
-      <button class="mdl-button mdl-button--icon mdl-js-button mdl-js-ripple-effect">
-        <i class="material-icons">copy</i>
-      </button>
-    </div>
+
   </div>
+
+  <div style="clear:both;"></div>
 
   <hr />
 
@@ -230,8 +258,8 @@ export class VaultFolder {
       </tr>
     </thead>
     <tbody *ngIf="logs">
-      <tr *ngFor="#log of logs">
-        <td>{{log.time | date:'d/M H:mm'}}</td>
+      <tr *ngFor="#log of logs; #i=index">
+        <td>{{log.time | date:'d/M H:mm:ss'}}</td>
         <td>{{log.auth.display_name}}</td>
         <td class="op-{{log.request.operation}}">{{log.request.operation}}</td>
         <td>{{log.request.path}}</td>
@@ -250,7 +278,10 @@ export class App {
   folders:any[] = [];
   token: string;
   logs: any;
-  ngOnInit() { this.loadToken(); this.getItems(this.tree); this.getLog(); }
+  editMode: boolean = false;
+  currentFile:VaultFolder = null;
+  backupWhileEditing:any;
+  ngOnInit() { this.loadToken(); this.getItems(this.tree); this.getLog(); setInterval(() => this.getLog(), 10000); }
 
   getLog() {
     var data = require('fs').readFileSync(vaultFolder() + 'audit.log', {encoding: 'utf8'});
@@ -258,14 +289,62 @@ export class App {
     data = JSON.parse(data);
 
     var newLogs = [];
-    data.data.forEach(function(entry) {
+    data.data.forEach(entry => {
       if (entry.type === 'request') {
         entry.time = new Date(entry.time);
-        newLogs.push(entry);
+        if (entry.request.operation !== 'list') {
+          newLogs.push(entry);
+        }
       }
     });
 
+    newLogs.sort((a, b) => {
+      if (a.time > b.time) {
+        return -1;
+      } else if (a.time < b.time) {
+        return 1;
+      }
+
+      return 0;
+    });
+
     this.logs = newLogs;
+  }
+
+  startEdit() {
+    this.backupWhileEditing = [];
+
+    for (let i in this.singleData) {
+      this.backupWhileEditing[i] = {};
+      this.backupWhileEditing[i].key = this.singleData[i].key;
+      this.backupWhileEditing[i].data = this.singleData[i].data;
+      this.backupWhileEditing[i].icon = this.singleData[i].icon;
+    }
+
+    this.editMode = true;
+  }
+
+  cancelEdit() {
+    this.singleData = [];
+
+    for (let i in this.backupWhileEditing) {
+      this.singleData[i] = {};
+      this.singleData[i].key = this.backupWhileEditing[i].key;
+      this.singleData[i].data = this.backupWhileEditing[i].data;
+      this.singleData[i].icon = this.backupWhileEditing[i].icon;
+    }
+
+    this.editMode = false;
+  }
+
+  saveEdit() {
+    this._vaultService.saveItem(this.currentFile.fullPath(), this.singleData, this.token)
+                     .subscribe(
+                       data => {
+                         this.getLog();
+                         this.editMode = false;
+                       },
+                       error =>  this.errorMessage = <any>error);
   }
 
   getItems(currentFolder:VaultFolder, saveToken:boolean = false) {
@@ -289,6 +368,8 @@ export class App {
                      .subscribe(
                        data => {
                          this.singleData = data;
+
+                         this.getLog();
                        },
                        error =>  this.errorMessage = <any>error);
   }
@@ -298,6 +379,7 @@ export class App {
     if (folder.folder) {
       this.getItems(folder);
     } else {
+      this.currentFile = folder;
       this.getItem(folder);
     }
 
@@ -312,6 +394,13 @@ export class App {
       require('fs').writeFileSync(vaultFolder() + '.vault-manager-token', '');
     }
     this.token = require('fs').readFileSync(vaultFolder() + '.vault-manager-token', {encoding: 'utf8'});
+  }
+
+  clipboard(text) {
+    if (!this.editMode) {
+      var clipboard = require('electron').clipboard;
+      clipboard.writeText(text);
+    }
   }
 
 }
